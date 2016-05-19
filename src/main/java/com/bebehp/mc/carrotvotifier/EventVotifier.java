b744a57496b8220438dd31805ea97b67d7cead57
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-05-20 01:57:51 +0900

    Commit

--- src/main/java/com/bebehp/mc/carrotvotifier/EventVotifier.java
+++ src/main/java/com/bebehp/mc/carrotvotifier/EventVotifier.java
@@ -3,7 +3,6 @@ package com.bebehp.mc.carrotvotifier;
 import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.model.VotifierEvent;
 
-import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.item.EntityFireworkRocket;
 import net.minecraft.entity.player.EntityPlayer;
@@ -13,36 +12,33 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.management.ServerConfigurationManager;
-import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.IChatComponent;
 import net.minecraft.world.World;
 
 public class EventVotifier {
 	public static final EventVotifier INSTANCE = new EventVotifier();
 
+	private final MinecraftServer s = MinecraftServer.getServer();
+
 	@SubscribeEvent
 	public void onVoteEvent(final VotifierEvent event) {
 		Reference.logger.info("VOTE!!");
 		final Vote vote = event.getVote();
-		final String name1 = vote.getUsername();
-		final String name = name1.replaceAll(" ", "");
+		//final String name = vote.getUsername();
+		final String name = vote.getUsername();
+		//		final String name = name1.replaceAll(" ", "");
 		final World world = MinecraftServer.getServer().getEntityWorld();
+		final IChatComponent c1 = ChatUtil.byText(String.format("§e%sが投票しました。引き換えアイテムをゲット！", name));
+		final IChatComponent c2 = ChatUtil.byJson("{\"text\":\"手に持ったTConstructのツールがレベルアップ！投票はこちら\",\"underlined\":true,\"color\":\"green\",\"clickEvent\":{\"action\":\"open_url\",\"value\":\"https://minecraft.jp/servers/mc.bebehp.com/vote\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"§1クリック§7して §eJapan Minecraft Servers §7で §6FruitServer §7の投票をしよう！\"}}");
+		ChatUtil.sendServerChat(c1, c2);
+		this.s.getCommandManager().executeCommand(this.s, String.format("/give %s minecraft:spawn_egg 1 0 {display:{Name:\"投票引き換え券\",Lore:[\"Spawn地点で引き換えよう！\"]},ench:[{id:21,lvl:1}]}", name));
+		this.s.getCommandManager().executeCommand(this.s, String.format("/leveluptool %s", name));
 		final EntityPlayer player = world.getPlayerEntityByName(name);
-		sendServerChat(String.format("§e%sが投票しました。ありがとうございます。", name));
 		if (player != null) {
 			fireworksPlayer(world, player);
 		}
 	}
 
-	public static void sendServerChat(final String msg) {
-		final ServerConfigurationManager sender = FMLCommonHandler.instance().getMinecraftServerInstance()
-				.getConfigurationManager();
-
-		final String[] linemsg = msg.split("\n");
-		for (final String line : linemsg) {
-			sender.sendChatMsg(new ChatComponentText(line));
-		}
-	}
 
 	public static void fireworksPlayer(final World world, final EntityPlayer player) {
 		world.spawnEntityInWorld(new EntityFireworkRocket(
