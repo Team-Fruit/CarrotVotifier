d79e91996522b2f6aa61d76678a84f95b695c7e3
sjcl <fruit@bebehp.com>
2017-03-05 12:56:38 +0900

    Fireworks

--- src/main/java/com/bebehp/mc/carrotvotifier/EventVotifier.java
+++ src/main/java/com/bebehp/mc/carrotvotifier/EventVotifier.java
@@ -7,10 +7,17 @@ import com.vexsoftware.votifier.model.VotifierEvent;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.ServerTickEvent;
+import net.minecraft.entity.item.EntityFireworkRocket;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemDye;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.IChatComponent;
+import net.minecraft.world.World;
 
 public class EventVotifier {
 	public static final EventVotifier INSTANCE = new EventVotifier();
@@ -40,6 +47,11 @@ public class EventVotifier {
 
 		// Get Voter EntityPlayer
 		final EntityPlayerMP player = this.s.getConfigurationManager().func_152612_a(name);
+		final World world = player.getEntityWorld();
+
+		// Fireworks
+		if (player!=null)
+			fireworksPlayer(world, player);
 
 		// Notice
 		IChatComponent c0 = ChatUtil.byText("");
@@ -59,38 +71,35 @@ public class EventVotifier {
 		ChatUtil.sendServerChat(c0, c1);
 	}
 
-	/*
-		public static void fireworksPlayer(final World world, final EntityPlayer player) {
-			world.spawnEntityInWorld(new EntityFireworkRocket(
-					world,
-					player.getPlayerCoordinates().posX,
-					player.getPlayerCoordinates().posY,
-					player.getPlayerCoordinates().posZ,
-					makeFireworks()
-					));
-		}
-	
-		public static ItemStack makeFireworks() {
-			final NBTTagCompound explosion = new NBTTagCompound();
-			explosion.setByte("Type", (byte)2);
-			explosion.setByte("Trail", (byte)1);
-			explosion.setIntArray("Colors", ItemDye.field_150922_c);
-	
-			final NBTTagList nbttaglist = new NBTTagList();
-			nbttaglist.appendTag(explosion);
-			nbttaglist.appendTag(explosion);
-			nbttaglist.appendTag(explosion);
-	
-			final NBTTagCompound nbt_fireworks = new NBTTagCompound();
-			nbt_fireworks.setTag("Explosions", nbttaglist);
-			nbt_fireworks.setByte("Flight", (byte)1);
-	
-			final NBTTagCompound nbt = new NBTTagCompound();
-			nbt.setTag("Fireworks", nbt_fireworks);
-	
-			final ItemStack itemstack = new ItemStack(Items.firework_charge);
-			itemstack.setTagCompound(nbt); return itemstack;
-		}
-		*/
+	public static void fireworksPlayer(final World world, final EntityPlayer player) {
+		world.spawnEntityInWorld(new EntityFireworkRocket(
+				world,
+				player.getPlayerCoordinates().posX,
+				player.getPlayerCoordinates().posY,
+				player.getPlayerCoordinates().posZ,
+				makeFireworks()));
+	}
 
+	public static ItemStack makeFireworks() {
+		final NBTTagCompound explosion = new NBTTagCompound();
+		explosion.setByte("Type", (byte) 2);
+		explosion.setByte("Trail", (byte) 1);
+		explosion.setIntArray("Colors", ItemDye.field_150922_c);
+
+		final NBTTagList nbttaglist = new NBTTagList();
+		nbttaglist.appendTag(explosion);
+		nbttaglist.appendTag(explosion);
+		nbttaglist.appendTag(explosion);
+
+		final NBTTagCompound nbt_fireworks = new NBTTagCompound();
+		nbt_fireworks.setTag("Explosions", nbttaglist);
+		nbt_fireworks.setByte("Flight", (byte) 1);
+
+		final NBTTagCompound nbt = new NBTTagCompound();
+		nbt.setTag("Fireworks", nbt_fireworks);
+
+		final ItemStack itemstack = new ItemStack(Items.firework_charge);
+		itemstack.setTagCompound(nbt);
+		return itemstack;
+	}
 }
